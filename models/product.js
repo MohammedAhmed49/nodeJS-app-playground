4a99cdd26b0f501255fc85daa133737635d44470
Mohammed Ahmed <mohammed_ahmed4949@yahoo.com>
2023-12-11 00:20:30 +0200

    Small enhancement

--- models/product.js
+++ models/product.js
@@ -2,32 +2,32 @@ const path = require("path");
 const fs = require("fs");
 
 const filePath = path.join(path.dirname(require.main.filename), "data", "products.json");
+
+const getProductsFromFile = (callbackFn) => {
+    fs.readFile(filePath, (error, data) => {
+        if (error) {
+            callbackFn([]);
+        } else {
+            callbackFn(JSON.parse(data));
+        }
+    })
+}
 class Product {
     constructor(title) {
         this.title = title;
     }
 
     save() {
-        fs.readFile(filePath, (error, data) => {
-            let products = [];
-            if(!error) {
-                products = JSON.parse(data);
-            }
+        getProductsFromFile(products => {
             products.push(this);
             fs.writeFile(filePath, JSON.stringify(products), (error) => {
                 console.log(error);
-            })
+            });
         })
     }
 
     static fetchAllProducts(callbackFn) {
-        fs.readFile(filePath, (error, data) => {
-            if (error) {
-                callbackFn([]);
-            } else {
-                callbackFn(JSON.parse(data));
-            }
-        })
+        getProductsFromFile(callbackFn);
     }
 }
 
