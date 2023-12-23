049b6f01f57974a99b03ebf69fe16e2bdab583a1
Mohammed Ahmed <mohammed_ahmed4949@yahoo.com>
2023-12-23 17:26:55 +0200

    Restructure views

--- controllers/products.js
+++ controllers/products.js
@@ -1,7 +1,7 @@
 const Product = require("../models/product");
 
 const getAddProduct = (req, res, next) => {
-    res.render("add-product", {pageTitle: "Add Product", page: "add-product"})
+    res.render("admin/add-product", {pageTitle: "Add Product", page: "add-product"})
 };
 
 const postAddProduct = (req, res, next) => {
@@ -12,7 +12,7 @@ const postAddProduct = (req, res, next) => {
 
 const getProducts = (req, res, next) => {
     Product.fetchAllProducts((products) => {
-        res.render("shop", {pageTitle: "My Shop", products: products, page: "shop"});
+        res.render("shop/product-list", {pageTitle: "My Shop", products: products, page: "shop"});
     });
     // res.render("shop", {pageTitle: "My Shop", products: [], page: "shop"});
 }
